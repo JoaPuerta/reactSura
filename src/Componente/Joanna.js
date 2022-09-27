e13b7111f2a7c2c1f0efb634c7c2b218f092fbb5
407 <407@academicos.cesde.edu.co>
2022-09-27 17:35:52 -0500

    se crea app de react

---
+++ src/Componente/Joanna.js
@@ -0,0 +1,12 @@
+export function Joanna(){
+
+    let nombre="Joanna"
+
+    return(
+        <>
+            <h1>HOLA REACT SOY {nombre}</h1>
+            <hr/>
+        </>
+       
+    )
+}
---
+++ src/Sipnosis/Sipnosis.js
@@ -0,0 +1,8 @@
+export function Sipnosis (){
+    return(
+        <>
+          Después de una infancia difícil, donde fue pasando de orfanato a hogares de acogida, es enviada por error a vivir con una solterona y su hermano. Cuando cumple 13 años, Anne va a conseguir transformar su vida y el pequeño pueblo donde vive gracias a su fuerte personalidad, intelecto e imaginación.
+            
+        </>
+    )
+}
